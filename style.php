33e2ec313c27be96a29f51732a01852dd7f68445
renjiraND <naufhalrenjira@gmail.com>
2018-04-21 00:24:11 +0700

    updated test.php better ui lessbugs

--- style.php
+++ style.php
@@ -1,35 +1,41 @@
 <?php
 	/*** set the content type header ***/
 	header("Content-type: text/css");
-	$font_family = 'Arial, Helvetica, Lobster, sans-serif';
+	$font_family = 'Arial, Helvetica, Allerta Stencil, sans-serif';
 	$font_size = '0.7em';
 	$border = '1px solid';
 ?>
 
-h1 {
-    color: Black;
+.content h1 {
+    background-color : black;
+    color: white;
     text-align: right;
-	margin : 20px;
-	font-family : Lobster;
+    padding : 10px;
+    margin-top : -5px;
+    margin-left: -10px; 
+	font-family : Allerta Stencil;
+    font-size : 60px;
+}
+
+.content .w3-card-4 .tweet {
+    font-family: Verdana;
 }
 
 .button {
-    background-color: #FF3333;
-    border: 1px solid #000000;
-	border-radius : 4px;
-    color: white;
+    border: 0px solid #885555;
+    background-color: #552222;
     padding: 16px 32px;
     text-align: center;
     text-decoration: none;
     display: inline-block;
     font-size: 16px;
-    -webkit-transition-duration: 0.4s; /* Safari */
+    -webkit-transition-duration: 0.4s;
     transition-duration: 0.4s;
     cursor: pointer;
 }
 
 .button:hover {
-    background-color: #FFFFFF; /* Green */
+    background-color: #FF1111;
     color: black;
 }
 
@@ -45,12 +51,12 @@ body {
 /* Style the side navigation */
 .sidenav {
     height: 100%;
-    width: 200px;
+    width: 210px;
     position: fixed;
     z-index: 1;
     top: 0;
     left: 0;
-    background-color: #800000;
+    background-color: #400000;
     overflow-x: hidden;
 }
 
@@ -58,14 +64,22 @@ body {
 .sidenav button {
     color: white;
     padding: 16px;
+    margin: 3px;
 	width: 200px;
     display: block;
 }
 
 .sidenav label {
-    font-family: Verdana;
-    font-size: 20px;
+    font-family: Consolas;
+    font-size: 15px;
 }
+
+.sidenav input {
+    background-color: transparent;
+    color: white;
+    border: transparent; 
+}
+
 
 /* Change color on hover */
 .sidenav a:hover {
--- test.php
+++ test.php
@@ -5,12 +5,15 @@
 		<title>SpamDetectorApp - TubzSteema</title>
 		<link rel="stylesheet" href="style.php" media="screen">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Allerta+Stencil">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
 	</head>
 	<body>
 		<div class="sidenav">
 			<form method="post">
-				<label class="w3-text-white " font-family="Verdana"><b>Input Query</b></label>
+				<label class="w3-text-white"><b>Input Query</b></label>
 				<input class="w3-input w3-animate-input w3-border" autocomplete="off" name="Konteks" type="text" placeholder="insert query here">
 				<label class="w3-text-white"><b>Detection Algorithms</b></label>
 				<button class="button" name="KMPButton">KMP</button>
@@ -21,21 +24,23 @@
 
 		<div class="content">
 			<h1>Spam Detector App</h1>
-			<hr style="height:30px">
-			<?php
-				if (file_exists("data.txt")) {
-					$data = json_decode(file_get_contents("data.txt"), true);
-					$text = $data['text'];
-					//print_r($text);
-					$i = 0;
-					while($i < sizeof($text)):
-						echo 
-						$text[$i]['test'];
-						echo "<br>";
-						$i += 1;
-					endwhile;
-				}
-			?>
+			<div class="w3-card-4">
+					<?php
+						if (file_exists("data.txt")) {
+							$data = json_decode(file_get_contents("data.txt"), true);
+							$text = $data['text'];
+							//print_r($text);
+							$i = 0;
+							while($i < sizeof($text)):
+								echo "<header class=\"w3-container w3-green\"><h6>Not a spam</h6></header>";
+								echo "<br>";
+								echo "<div class=\"w3-container tweet\">",$text[$i]['test'],"</div>";
+								echo "<hr>";
+								$i += 1;
+							endwhile;
+						}
+					?>
+			</div>
 		</div>
 	</body>
 </html>
